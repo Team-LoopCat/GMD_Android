f44b78c121265e1505490f5162b9423c99fd1402
Hong Seoeun <hseun0732@dsm.hs.kr>
2023-12-25 01:31:06 +0900

    add :: serverApi

---
+++ app/src/main/java/com/loopcat/gmd/ServerApi.kt
@@ -0,0 +1,25 @@
+package com.loopcat.gmd
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface ServerApi {
+    @POST("/user/login")
+    fun login(@Body request : LoginRequest) : Call<LoginResponse>
+
+    @GET("/chack/list/{grade}")
+    fun getList(
+        @Path("grade") grade : Int,
+        @Header("access_token") token : String,
+    ) : Call<BoxResponse>
+
+    @GET("/chack/info/{boxID}")
+    fun getBoxInfo(
+        @Path("boxID") boxId : Int,
+        @Header("acccess_token") token: String
+    ) : Call<InfoResponse>
+}
