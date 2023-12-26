afdd8e8f6a513acf49f376cccfe3a7cbda514572
Hong Seoeun <hseun0732@dsm.hs.kr>
2023-12-26 11:27:04 +0900

    add :: PETCH status

--- app/src/main/java/com/loopcat/gmd/Request.kt
+++ app/src/main/java/com/loopcat/gmd/Request.kt
@@ -1,3 +1,5 @@
 package com.loopcat.gmd
 
 data class LoginRequest(val userName : String, val password : String)
+
+data class ChangeStatusRequest(val phone: Boolean, val personalLaptop: Boolean, val schoolLaptop: Boolean, val tablet: Boolean)
--- app/src/main/java/com/loopcat/gmd/ServerApi.kt
+++ app/src/main/java/com/loopcat/gmd/ServerApi.kt
@@ -4,6 +4,7 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -22,4 +23,11 @@ interface ServerApi {
         @Path("boxID") boxId : Int,
         @Header("acccess_token") token: String
     ) : Call<InfoResponse>
+
+    @PATCH("/chack/{boxID}")
+    fun changeStatus(
+        @Path("boxID") boxId: Int,
+        @Header("access_token") token: String,
+        @Body request: ChangeStatusRequest
+    ): Call<Void>
 }
